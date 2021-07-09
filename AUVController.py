485b40d3e5f1e94c324b0e2a660263197cc25db3
Robert White <rjwhitenh@outlook.com>
2021-07-09 11:28:50 -0400

    Added heading method

--- AUVController.py
+++ AUVController.py
@@ -2,3 +2,6 @@ class AUVController(object):
     def __init__(self, auv_state):
         self.__auv_state = auv_state
         self.__desired_heading = 0.0
+
+    def get_desired_heading(self)
+        return self.__desired_heading
