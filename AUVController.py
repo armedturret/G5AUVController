6fb0639fe26e48d77ebf76f9d4c35b5c0e8ae264
Robert White <rjwhitenh@outlook.com>
2021-07-09 11:32:47 -0400

    Fixed small typo

--- AUVController.py
+++ AUVController.py
@@ -3,5 +3,5 @@ class AUVController(object):
         self.__auv_state = auv_state
         self.__desired_heading = 0.0
 
-    def get_desired_heading(self)
+    def get_desired_heading(self):
         return self.__desired_heading
